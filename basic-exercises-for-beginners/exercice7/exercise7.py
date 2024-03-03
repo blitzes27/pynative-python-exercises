f1872dec5f65fa2dc39f174d3406891bd5b23cf2
Meron Tedros <meron.tedros@gmail.com>
2024-03-03 18:19:41 +0100

    tillägg av uppgift

---
+++ basic-exercises-for-beginners/exercice7/exercise7.py
@@ -0,0 +1,4 @@
+str_x = "Emma is good developer. Emma is a writer"
+print(f"Strängen är: {str_x}\n")
+rakna = str_x.count("Emma")
+print(f"Förekomsten av Emma i strängen är: {rakna} antal gånger\n")
