eca720db9926d8c74d3c2a120c9b207b8b46a231
Meron Tedros <meron.tedros@gmail.com>
2024-03-15 16:59:49 +0100

    tillägg

---
+++ basic-exercises-for-beginners/exercise12/exercise12.py
@@ -0,0 +1,15 @@
+def skatteverket(*lon):
+    skatt_total = []  # lista för att spara total skatt för varje lön
+    utnyttjad_
+    resultat = 0
+    skattesats = 0  # Start värde på skattesats
+    for i in lon:
+        if i in lon[1] <= 10000:
+            print(f"skattesats är: {skattesats}% på lön {i} kr")
+            resultat += i
+        return resultat
+
+
+skatteverket(10000)
+skatteverket(15000)
+skatteverket(20000)
